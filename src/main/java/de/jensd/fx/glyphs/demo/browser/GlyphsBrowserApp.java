1228617669c04ac5478e7aecdf3dc3ab5e46b6ec
Jens Deters <jens.deters@codecentric.de>
2016-08-11 15:09:09 +0200

    added copyright hint

--- src/main/java/de/jensd/fx/glyphs/demo/browser/GlyphsBrowserApp.java
+++ src/main/java/de/jensd/fx/glyphs/demo/browser/GlyphsBrowserApp.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2015 Jens Deters http://www.jensd.de
+ * Copyright (c) 2016 Jens Deters http://www.jensd.de
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
  * License. You may obtain a copy of the License at
